dfe0bd4c9d43349f04efd9437e462d54d6147a68
Jacky Royer <kiky270281@gmail.com>
2025-10-05 14:45:46 +0200

    feat: add debug doc

---
+++ build-debug/CMakeFiles/hangman_exec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hangman_exec.dir/src/main.cpp.o"
+  "CMakeFiles/hangman_exec.dir/src/main.cpp.o.d"
+  "bin/hangman_exec"
+  "bin/hangman_exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hangman_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
